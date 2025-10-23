dcbc31f7fc7411c0e25ad6f19eaa2e704f3b11f5
Alexey Konyshev <akonyshev@nnccompany.ru>
2025-10-23 19:22:43 +0300

    Fix debouncer and implement toggle led

--- src/main.vhd
+++ src/main.vhd
@@ -12,7 +12,11 @@ end entity top_module;
 
 architecture structural of top_module is
 -- variables
-signal debounced_out : std_logic;
+signal button_clean : std_logic;
+signal button_prev : std_logic := '0';
+
+signal led_state : std_logic := '0';
+
 component debouncer is 
      port (
             clk     : in  std_logic;
@@ -30,11 +34,25 @@ DEBOUNCE_INST : debouncer
         clk     => clk,
         reset_n => reset_n,
         button => button,
-        debounced_out => debounced_out
+        debounced_out => button_clean
     );
         
 
+LED_TOGGLE : process(clk, reset_n)
+begin
+    if reset_n = '1' then
+        button_prev <= '0';
+        led_state <= '0';
+    elsif rising_edge(clk) then
+        button_prev <= button_clean;
+        if button_prev = '0' and button_clean = '1' then
+            led_state <= not led_state;
+        end if;
+    end if;
+end process;
+
+
     -- Connect internal signals to output ports (or further processing)
-    led_out(0) <= debounced_out;
+    led_out(0) <= led_state;
 
 end architecture structural;
